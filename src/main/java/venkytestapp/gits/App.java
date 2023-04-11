b7512224244fbc4ccc7241a3eed0d82223260798
Admin <Admin@DESKTOP-M2C68SM.syenainfosoft.com>
2023-04-10 22:23:17 -0700

    ss

--- src/main/java/venkytestapp/gits/App.java
+++ src/main/java/venkytestapp/gits/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-     
+    	System.out.println( "hello venkatesh" );
     	System.out.println( "welcome to github session" );
     }
 }
